d1fee7b9a8fe6e6c791b32b15f84e12bdc3ff07c
Logan Clark <cla20067@byui.edu>
2022-09-21 13:46:16 -0600

    Add server.js and modified package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.listen(port, async ()=>{
+    console.log('listening on port' +port);
+});
+
+app.get('/', (req,res)=>{
+    res.send('Hello World!')
+});
