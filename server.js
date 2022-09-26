2f7a796d9e727f373ed29ed7353f087a76b52d08
Logan Clark <cla20067@byui.edu>
2022-09-26 13:41:59 -0600

    Added code to server.js and signin.cmd

--- server.js
+++ server.js
@@ -13,3 +13,20 @@ app.get('/', (req,res)=>{
 });
 
 app.use(bodyParser.json());
+
+app.post("/login", (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginPassword);
+    // res.send("Who are you?");
+
+    if (loginEmail == "testerG@gmail.com" && loginPassword == "P@ssw0rd"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized error
+        res.send("Invalid user or password");
+    }
+})
--- signin.cmd
+++ signin.cmd
@@ -1,1 +1,3 @@
-curl -v -d "@signin.json" -X POST -H "Content-Type:application/text" http://localhost:3000/login
+curl -v -d "@signin.json" -X POST -H "Content-Type:application/JSON" https://dev.stedi.me/login
+
+@REM http://localhost:3000
