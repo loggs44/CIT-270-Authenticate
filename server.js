ff21e4b4e5126d666a7327257d902a4c83794994
Logan Clark <cla20067@byui.edu>
2022-12-02 12:50:42 -0700

    Added redis.yaml

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const md5 = require('md5');
 
 const redisClient = createClient(
     {
-    url:"redis://default@34.136.171.243:6379",
+    url:`redis://default:${process.env.REDIS_PASS}@redis-stedi-logan:6379`,
     }
 );
 
